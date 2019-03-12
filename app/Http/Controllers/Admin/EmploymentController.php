811e610775a96af4a46905e7376942903de1e6d9
Nam-atx <namrata.atxlearning@gmail.com>
2019-03-12 17:30:35 +0530

    update app/Http/Controllers/Admin/EmploymentController.php

--- app/Http/Controllers/Admin/EmploymentController.php
+++ app/Http/Controllers/Admin/EmploymentController.php
@@ -252,6 +252,7 @@ class EmploymentController extends Controller
         $employment->license=$request->input('license');
         $employment->need_call=$request->input('need_call');
         $employment->resume=$request->input('resume');
+        $employment->source=$request->input('source');
         //$employment->longitude=$response['longitude'];
         //$employment->latitude=$response['latitude'];
         $employment->save();
@@ -339,15 +340,16 @@ class EmploymentController extends Controller
                    // echo '==>'.$key.'<==<br>';
                     continue;
                 }
+                
                 //DB::enableQueryLog();    
                 $emp=Employment::where('email','=',$row['email_address'])->first();
                 //dd(DB::getQueryLog()); exit;
                 
 
                 if($emp){ 
-echo $row['email_address'].">>".$emp->email."<br />";
+
                 $data=['application_date'=>$emp->application_date,'title'=>$emp->title, 'first_name'=>$emp->first_name, 'last_name'=>$emp->last_name,'email'=>$emp->email,'phone'=>$emp->phone,'cell_number'=>$emp->cell_number,'best_time_to_call'=>$emp->best_time_to_call,'street1'=>$emp->street1,'street2'=>$emp->street2,'city'=>$emp->city,'state'=>$emp->state,'zipcode'=>$emp->zipcode,'country'=>$emp->country,'position'=>$emp->position,'days_available'=>$emp->days_available,'license'=>mb_convert_encoding($emp->license, 'UTF-8'),'need_call'=>$emp->need_call,'resume'=>$emp->resume,'source'=>$emp->source];                    
-                //Emphistory::create($data);
+                Emphistory::create($data);
 
                 $emp->application_date=$application_date;
                 $emp->title=$row['title'];
@@ -375,7 +377,7 @@ echo $row['email_address'].">>".$emp->email."<br />";
                 }    
                 $emp->longitude='0';
                 $emp->latitude='0';
-                //$emp->save();
+                $emp->save();
 
                 } else {
 
@@ -393,11 +395,11 @@ echo $row['email_address'].">>".$emp->email."<br />";
                 else{
                     $source = '';
                 }    
-echo "test".$row['email_address']."<br />";
+
 
                 $data=['application_date'=>$application_date,'title'=>$row['title'], 'first_name'=>$row['first_name'], 'last_name'=>$row['last_name'],'email'=>$row['email_address'],'phone'=>$row['phone'],'cell_number'=>$row['cell_number'],'best_time_to_call'=>$row['best_time_to_call'],'street1'=>$row['address_street_address'],'street2'=>$row['address_street_address_line_2'],'city'=>$row['city'],'state'=>$row['state'],'zipcode'=>(strlen($row['zip_code'])==4)?'0'.$row['zip_code']:$row['zip_code'],'country'=>$row['address_country'],'position'=>$row['applying_for_position'],'days_available'=>$row['days_available'],'license'=>mb_convert_encoding($row['licenses_skills_training_awards'], 'UTF-8'),'need_call'=>$row['need_a_call_back'],'resume'=>$row['resume'],'source'=>$source,'longitude'=>'0','latitude'=>'0','email_status'=>$email_status];
 
-                //Employment::create($data);
+                Employment::create($data);
 
                 }
                 //sleep(1);
@@ -405,7 +407,7 @@ echo "test".$row['email_address']."<br />";
 
             });
         }
-        exit;
+        
         flash('Your file successfully import in database!!!')->success();
         return back();
     }
